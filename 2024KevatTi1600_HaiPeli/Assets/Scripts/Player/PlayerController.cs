e4d70a9e2f4e41a03e37e29bfd7fc8b3e9c8ec5c
Your Name <you@example.com>
2024-02-20 16:57:38 +0200

    pelaajan liikkuminen tehty ja ammos scripti luotu

--- 2024KevatTi1600_HaiPeli/Assets/Scripts/Player/PlayerController.cs
+++ 2024KevatTi1600_HaiPeli/Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float moveSpeed = 5f;
+    
+    private Rigidbody2D body;
+
+    private Master controls;
+
+    private Vector2 moveInput;
+
+    private void Awake() {
+        controls = new Master();
+        body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable() {
+        controls.Enable();    
+    }
+ 
+    private void OnDisable(){
+        controls.Disable();
+    }
+
+    private void FixedUpdate() {
+        Move();
+    }
+
+    private void Move()
     {
-        
+        moveInput = controls.Player.Move.ReadValue<Vector2>();
+        Vector2 movement = new Vector2(moveInput.x, moveInput.y) * moveSpeed * Time.fixedDeltaTime;
+        body.MovePosition(body.position + movement);
     }
 }
